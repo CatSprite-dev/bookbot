fc44bc6a0b78d58015070b1bebd6e03421248b91
ThePrimeagen <catspriteee@gmail.com>
2025-02-12 22:49:10 +0300

    Finished

--- main.py
+++ main.py
@@ -22,27 +22,37 @@ def count_characters(file_contents):
     return chars
 
 def dict_list(characters_count):
-    chars = []
+    dicts_in_list = []
     for k, v in characters_count.items():
-        chars.append({k: v})
-    return chars   
-
-def sort(results):
-    for i in results:
-        return i
-    
-
-
+        if k.isalpha() == True:
+            chars = {}
+            chars["Characters"] = k
+            chars["count"] = v
+            dicts_in_list.append(chars)
+    return dicts_in_list       
+   
+def sort(dicts_in_list):
+    return dicts_in_list["count"]
 
 def main():
     
     file_contents = full_text(path) 
     word_count = count_words(file_contents) 
     characters_count = count_characters(file_contents) # dict
-    results = dict_list(characters_count)
-
+    
+    dicts_in_list = dict_list(characters_count) # list
+    dicts_in_list.sort(reverse=True, key=sort)
+    
+    
+    
     print(f"--- Begin report of {path} ---")
     print(f"{word_count} words found in the document")
+    print()
+    for i in dicts_in_list:
+        print(f"The '{i.get("Characters")}' character was found {i.get("count")} times")
+    print("--- End report ---")
+    
+
 
 
 main()
